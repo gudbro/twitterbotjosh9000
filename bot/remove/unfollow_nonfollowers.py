08694917e7223fc3ca98cc1968a4ddd3b29e4f78
gudbro <kepoinmenfess@gmail.com>
2020-09-27 12:22:11 +0700

    twitbot

--- bot/remove/unfollow_nonfollowers.py
+++ bot/remove/unfollow_nonfollowers.py
@@ -8,13 +8,13 @@ from random import choice
 
 # ---------------------------------------------------------------------------- #
 # API key:
-api_key = getenv("CONSUMER_KEY")
+api_key = getenv("85ydEkXlBBciiAYLONlPMqVhk")
 # API secret key:
-api_secret = getenv("CONSUMER_SECRET")
+api_secret = getenv("QT2RlCWJCybSM98YCAwmjXpUkPGjJvnVA7vnqputXwAv6qLXR4")
 # Access token: 
-access_token = getenv("API_KEY")
+access_token = getenv("1305673102160875523-6bstDMBo5xu0UOdqea3mi5x0uKPSph")
 # Access token secret: 
-access_token_secret = getenv("API_SECRET")
+access_token_secret = getenv("7gOQNzknTQDDle6IForuCTpP6WIjRXLh0Y6BrTnSafjAZ")
 # ---------------------------------- Tweepy ---------------------------------- #
 # Tweepy 0Auth 1a authentication:
 auth = OAuthHandler(api_key, api_secret)
--- bot/status/models.py
+++ bot/status/models.py
@@ -9,8 +9,8 @@ app = Flask(__name__)
 host = getenv('HOST')
 port = getenv('PORT')
 # --------------------------------- DB Config -------------------------------- #
-app.config['SQLALCHEMY_DATABASE_URI'] = getenv('SQLALCHEMY_DATABASE_URI')
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = getenv('SQLALCHEMY_TRACK_MODIFICATIONS')
+app.config['SQLALCHEMY_DATABASE_URI'] = false  #getenv('SQLALCHEMY_DATABASE_URI')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = false #getenv('SQLALCHEMY_TRACK_MODIFICATIONS')
 db = SQLAlchemy(app)
 # ---------------------------------------------------------------------------- #
 migrate = Migrate(app, db)
